9d1d841c9b95d79a930c6a08f3a44e83cef46435
hackbymesk <sumeetkhillare10@gmail.com>
2021-04-05 16:33:22 +0530

    checkpoint code added and eclipse folder removed.

--- flink-quick/src/main/java/com/skhillare/flink/StreamingJob.java
+++ flink-quick/src/main/java/com/skhillare/flink/StreamingJob.java
@@ -16,18 +16,23 @@
  * limitations under the License.
  */
 
+
+//--port 9000 --checkpoint 5000
+//-c com.skhillare.flink.StreamingJob
+
 package com.skhillare.flink;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.util.Collector;
-
-
+import java.util.concurrent.TimeUnit;
 
 /**
  * Skeleton for a Flink Streaming Job.
@@ -46,18 +51,54 @@ import org.apache.flink.util.Collector;
 @SuppressWarnings("serial")
 public class StreamingJob {
 
-	public static void main(String[] args) throws Exception {
 
+	/** Data type for words with count. */
+	public static class WordWithCount {
+
+		public String word;
+		public long count;
+
+		public WordWithCount() {}
+
+		public WordWithCount(String word, long count) {
+			this.word = word;
+			this.count = count;
+		}
+
+		@Override
+		public String toString() {
+			return word + " : " + count;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		final String CLASS_NAME = StreamingJob.class.getSimpleName();
 
 		// the host and the port to connect to
 		final String hostname;
 		final int port;
+		// get the execution environment
+		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
 		try {
 			final ParameterTool params = ParameterTool.fromArgs(args);
-			hostname="localhost";
-			port=9000;
-//			hostname = params.has("hostname") ? params.get("hostname") : "localhost";
-//			port = params.getInt("port");
+//			hostname="localhost";
+//			port=9000;
+			hostname = params.has("hostname") ? params.get("hostname") : "localhost";
+			port = params.getInt("port");
+			// Configure checkpointing if interval is set
+			long cpInterval = params.getLong("checkpoint", TimeUnit.MINUTES.toMillis(1));
+			if (cpInterval > 0) {
+				CheckpointConfig checkpointConf = env.getCheckpointConfig();
+				checkpointConf.setCheckpointInterval(cpInterval);
+				checkpointConf.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+				checkpointConf.setCheckpointTimeout(TimeUnit.HOURS.toMillis(1));
+				checkpointConf.enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
+				env.getConfig().setUseSnapshotCompression(true);
+
+			}
+
+
 		} catch (Exception e) {
 			System.err.println(
 					"No port specified. Please run 'SocketWindowWordCount "
@@ -69,8 +110,6 @@ public class StreamingJob {
 			return;
 		}
 
-		// get the execution environment
-		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
 		// get input data by connecting to the socket
 		DataStream<String> text = env.socketTextStream(hostname, port, "\n");
@@ -99,29 +138,9 @@ public class StreamingJob {
 
 		// print the results with a single thread, rather than in parallel
 		windowCounts.print().setParallelism(1);
-
+		System.out.println("Starting on "+hostname+" port: "+String.valueOf(port)+"\n");
 		env.execute("Socket Window WordCount");
 
-	}
-
-
-	/** Data type for words with count. */
-	public static class WordWithCount {
-
-		public String word;
-		public long count;
-
-		public WordWithCount() {}
-
-		public WordWithCount(String word, long count) {
-			this.word = word;
-			this.count = count;
-		}
-
-		@Override
-		public String toString() {
-			return word + " : " + count;
-		}
 	}
 
 }
