91330b9f4344a880432a9d4d4ba51ac92c3bd5a9
hackbymesk <sumeetkhillare10@gmail.com>
2021-05-03 20:24:55 +0530

    Reducing State Added.

--- queryable-1.7.2/src/main/java/com/skhillare/flink/StreamingJob.java
+++ queryable-1.7.2/src/main/java/com/skhillare/flink/StreamingJob.java
@@ -16,10 +16,6 @@
  * limitations under the License.
  */
 
-
-//--port 9000 --checkpoint 5000
-//-c com.skhillare.flink.StreamingJob
-
 package com.skhillare.flink;
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
@@ -39,12 +35,10 @@ package com.skhillare.flink;
  * limitations under the License.
  */
 
-
-//--port 9000 --checkpoint 5000
-//-c com.skhillare.flink_avg.StreamingJob
 //./bin/flink run -c com.skhillare.flink.StreamingJob ~/flink-quick-1.0-SNAPSHOT.jar --port 9000 --checkpoint 5000
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.common.functions.RichFlatMapFunction;
 import org.apache.flink.api.common.state.*;
 import org.apache.flink.api.common.typeinfo.TypeHint;
@@ -52,7 +46,6 @@ import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.configuration.Configuration;
-import org.apache.flink.runtime.jobgraph.JobGraph;
 import org.apache.flink.shaded.curator.org.apache.curator.shaded.com.google.common.collect.Lists;
 import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
@@ -68,6 +61,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
+//Custom Exception to Quit
 class QuitValueState extends Exception{
 	QuitValueState(String m1,String inetAddress,int port) throws IOException {
 		super(m1);
@@ -82,79 +76,109 @@ class QuitValueState extends Exception{
 
 
 public class StreamingJob extends RichFlatMapFunction<Tuple2<String, Long>, Tuple2<String, Long>>  {
-	public final static String Q_NAME = "query";
-	public transient ValueState<Tuple2<String, Long>> sum;
-	public final static String List_Q_NAME = "list-query";
-	private ListState<Tuple2<String, Long>> listState;
+	//StateKey Common for all states.
 	public final static String statekey = "statekey";
+	//ValueState
+	public final static String Value_Q_NAME = "value-query";
+	public transient ValueState<Tuple2<String, Long>> sum;
+	//ListState
+	public final static String List_Value_Q_NAME = "list-query";
+	private ListState<Tuple2<String, Long>> listState;
+	//MapState
 	private transient MapState<String, Long> mapState;
-	public final static String Map_Q_NAME = "map-query";
-	public final static String Keyed_Q_NAME = "keyed-query";
+	public final static String Map_Value_Q_NAME = "map-query";
+	//ReducingState
+	private transient ReducingState<Tuple2<String,Long>> reducingState;
+	public final static String Reduce_Value_Q_NAME = "reduce-query";
+	//For flatmap operator
+	public final static String Keyed_Value_Q_NAME = "keyed-query";
+
+	//Function to print ListState
 	private void printstate() throws Exception{
 		ArrayList<Tuple2<String, Long>> listdata = Lists.newArrayList(listState.get());
-		System.out.println("liststate data");
+		System.out.println("ListState : ");
 		for (Tuple2<String, Long> data : listdata) {
 			System.out.print(data.f1.toString()+" ");
 		}
 		System.out.print("\n");
 
 	}
+
+	//Flatmap function to update values in states.
 	@Override
 	public void flatMap(Tuple2<String, Long> input, Collector<Tuple2<String, Long>> out) throws Exception {
+		//clear state
+		if (input.f1==-1){
+			sum.clear();
+			listState.clear();
+			mapState.clear();
+			reducingState.clear();
+			return;
+		}
+		//ValueState
+		Tuple2<String, Long> currentSum = sum.value();
+		currentSum.f1 += input.f1;
+		sum.update(currentSum);
+		System.out.println("**************\nValueState : "+(sum.value().f1));
+		//ListState
 		Iterable<Tuple2<String, Long>> state = listState.get();
 		if (null == state) {
 			listState.addAll(Collections.EMPTY_LIST);
 		}
-		if (input.f1==-1){
-			listState.clear();
-			sum.clear();
-			mapState.clear();
-			return;
-		}
 		listState.add(input);
 		printstate();
-
-		Tuple2<String, Long> currentSum = sum.value();
-		currentSum.f1 += input.f1;
-		sum.update(currentSum);
-		System.out.println("Current Sum: "+(sum.value().f1));
+		//MapState
 		mapState.put(statekey,sum.value().f1);
-		System.out.println("MapState:");
-		System.out.println(String.valueOf(mapState.get(statekey)));
-
+		System.out.println("MapState : "+mapState.get(statekey).toString());
+		//ReducingState
+		reducingState.add(Tuple2.of(statekey,input.f1));
+		System.out.println("ReducingState : "+reducingState.get().toString() );
+		//Collect for flatmap to query.
 		out.collect(new Tuple2<>(statekey, sum.value().f1));
 
 	}
+	//Initialize States
 	@Override
 	public void open(Configuration config) {
+		//ValueState
 		ValueStateDescriptor<Tuple2<String, Long>> descriptor =
 				new ValueStateDescriptor<>(
-						Q_NAME,
+						Value_Q_NAME,
 						TypeInformation.of(new TypeHint<Tuple2<String, Long>>() {}),
 						Tuple2.of(statekey, 0L));
-		descriptor.setQueryable(Q_NAME);
+		descriptor.setQueryable(Value_Q_NAME);
 		sum = getRuntimeContext().getState(descriptor);
-
-		ListStateDescriptor<Tuple2<String, Long>> listDis = new ListStateDescriptor<>(List_Q_NAME, TypeInformation.of(new TypeHint<Tuple2<String, Long>>() {
+		//ListState
+		ListStateDescriptor<Tuple2<String, Long>> listDis = new ListStateDescriptor<>(List_Value_Q_NAME, TypeInformation.of(new TypeHint<Tuple2<String, Long>>() {
 		}));
-		listDis.setQueryable(List_Q_NAME);
+		listDis.setQueryable(List_Value_Q_NAME);
 		listState = getRuntimeContext().getListState(listDis);
-
+		//MapState
 		MapStateDescriptor<String, Long> mapStateDes = new MapStateDescriptor<>(
-				Map_Q_NAME,
+				Map_Value_Q_NAME,
 				String.class,
 				Long.class);
-		mapStateDes.setQueryable(Map_Q_NAME);
+		mapStateDes.setQueryable(Map_Value_Q_NAME);
 		mapState = getRuntimeContext().getMapState(mapStateDes);
-
-
+		//ReducingState
+		ReducingStateDescriptor<Tuple2<String,Long>> reducingStateDescriptor = new ReducingStateDescriptor<>(
+				Reduce_Value_Q_NAME,
+				new ReduceFunction<Tuple2<String, Long>>() {
+					@Override
+					public Tuple2<String, Long> reduce(Tuple2<String, Long> stringLongTuple2, Tuple2<String, Long> t1) {
+						return Tuple2.of(statekey,stringLongTuple2.f1+t1.f1);
+					}
+				},TypeInformation.of(new TypeHint<Tuple2<String, Long>>() {})
+		);
+		reducingStateDescriptor.setQueryable(Reduce_Value_Q_NAME);
+		reducingState=getRuntimeContext().getReducingState(reducingStateDescriptor);
 
 	}
 	public static void main(String[] args) throws Exception {
 		final String hostname;
 		final Integer port;
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-
+		//Get Input i.e. hostname,port,time for checkpointing
 		try {
 			final ParameterTool params = ParameterTool.fromArgs(args);
 			hostname = params.has("hostname") ? params.get("hostname") : "localhost";
@@ -174,22 +198,24 @@ public class StreamingJob extends RichFlatMapFunction<Tuple2<String, Long>, Tupl
 							+ "--hostname <hostname> --port <port> --checkpoint <time>', where hostname (localhost by default) "
 							+ "and port is the address of the text server "+ "and chekpoint is the time for checkpointing");
 			System.err.println(
-					"To start a simple text server, run 'netcat -l <port>' and "
-							+ "type the input text into the command line");
+					"To start a simple server, run 'netcat -l <port>' and "
+							+ "type the numbers into the command line");
 			return;
 		}
 
-
 		DataStreamSource<String> inp = env.socketTextStream(hostname, port, "\n");
+		//ValueState descriptor used for query flatmap
 		ValueStateDescriptor<Tuple2<String, Long>> descriptor =
 				new ValueStateDescriptor<>(
-						Keyed_Q_NAME, // the state name
+						Keyed_Value_Q_NAME, // the state name
 						TypeInformation.of(new TypeHint<Tuple2<String, Long>>() {}), // type information
 						Tuple2.of(statekey, 0L));
+
+		//Get streaming data
 		inp.flatMap(new FlatMapFunction<String, Tuple2<String, Long>>() {
 			@Override
 			public void flatMap(String inpstr, Collector<Tuple2<String, Long>> out) throws Exception{
-
+				//Split numbers from string
 				for (String word : inpstr.split("\\s")) {
 					try {
 						if(word.equals("quit")){
@@ -198,24 +224,26 @@ public class StreamingJob extends RichFlatMapFunction<Tuple2<String, Long>, Tupl
 						if(word.equals("clear")){
 							word="-1";
 						}
-
+						//Collect statekey and number
 						out.collect(Tuple2.of(statekey, Long.valueOf(word)));
 					}
+					//Input is not number
 					catch ( NumberFormatException e) {
 						System.out.println("Enter valid number: "+e.getMessage());
-					}catch (QuitValueState ex){
+					}
+					//Quit
+					catch (QuitValueState ex){
 						System.out.println("Quitting!!!");
 					}
 				}
 			}
-		}).name("Input Conversion").keyBy(0).flatMap(new StreamingJob()).name("Store in State")
+		}).name("Flatmap for input conversion").keyBy(0).flatMap(new StreamingJob()).name("Flatmap to store in state")
 				.keyBy(0)
-				.asQueryableState(Keyed_Q_NAME,descriptor);
-		JobGraph jobGraph = env.getStreamGraph().getJobGraph();
-//		System.out.println("[info] Job ID: " + jobGraph.getJobID());
-		System.out.println("Running!!!");
+				//Set flatmap keyed stream Queryable.
+				.asQueryableState(Keyed_Value_Q_NAME,descriptor);
+		System.out.println("Sum Calculation Job Running!!!");
 		env.execute("Sum Job");
-	}
 
+	}
 
 }
